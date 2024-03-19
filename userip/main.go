a95e8a3956a901c257abcc142fa9f66aaf0ec4d0
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-03-19 16:49:31 +0530

    added userInput

---
+++ userip/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Welcome to user input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our pizza: ")
+
+	// comma ok || err err
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("Type of this rating is %T, ", input)
+
+}
